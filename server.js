bf57845799b08148e4005c2256d6681a5410c2ce
Stama Eleazar <eleoatam10@gmail.com>
2019-09-15 23:02:33 -0500

    express. listening on port

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 var express = require('express');
 var path = require('path');
 
+var app = express();
+
+var port = 3000;
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+   
+  app.listen(port, function(){
+      console.log('listening on: '+port)
+  })
+
+  
