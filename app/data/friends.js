8814cd0a87c6aae9709dac5afa6bf3c8f2b905d1
Stama Eleazar <eleoatam10@gmail.com>
2019-09-16 21:06:59 -0500

    added placeholder data to firends.js

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+    {
+        name: 'John Smith',
+        picture: 'https://randomuser.me/api/portraits/men/91.jpg',
+        rating: [
+            '4',
+            '3',
+            '4',
+            '4',
+            '2',
+            '4',
+            '3',
+            '4',
+            '5',
+            '4',
+        ]
+    },
+    {
+        name: 'Evelyn Mathew',
+        picture: 'https://randomuser.me/api/portraits/women/85.jpg',
+        rating: [
+            '2',
+            '3',
+            '2',
+            '2',
+            '2',
+            '2',
+            '3',
+            '2',
+            '5',
+            '2',
+        ]
+    },
+
+
+
+
+]
