4235e43214fa507a2d966a7e3afc39232d4cc110
Stama Eleazar <eleoatam10@gmail.com>
2019-09-16 20:57:56 -0500

    html routes completed

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,10 +7,10 @@ module.exports = function(app){
    
 
     app.get('/survey', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/survey.html'))
+        res.sendFile(path.join(__dirname, '/../public/survey.html'))
     });
 
     app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, '../public/home.html'));
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
     });
 };
